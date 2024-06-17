2772a67f1bee63b5c2a83808f457bf915dcb3157
ltdoan2004 <doanh25032004@gmail.com>
2024-06-17 14:21:13 +0700

    push

--- src/attention.py
+++ src/attention.py
@@ -40,6 +40,48 @@ class SelfAttention(nn.Module):
         output = self.out_proj(output)
 
         return output
+    
+    class CrossAttentionBlock(nn.Module):
+        def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias = True, out_proj_bias = True):
+            super().__init__()
+            self.q_proj = nn.Linear(d_embed, d_embed, bias = in_proj_bias)
+            self.k_proj = nn.Linear(d_cross, d_embed, bias = in_proj_bias)
+            self.v_proj = nn.Linear(d_cross, d_embed, bias = in_proj_bias)
+            self.out_proj = nn.Linear(d_embed, d_embed, bias = out_proj_bias)
+
+            self.n_heads = n_heads
+            self.d_head = d_embed // n_heads
+
+        def forward(self, x, y) -> torch.Tensor:
+            x_shape = x.shape
+
+            bs , seg_length, embed_dim = x_shape
+            intermim_shape = (bs, -1, self.n_heads, self.d_head)
+
+            q = self.q_proj(x)
+            k = self.k_proj(y)
+            v = self.v_proj(y)
+
+            q = q.view(intermim_shape).transpose(1,2)
+            k = k.view(intermim_shape).transpose(1,2)
+            v = v.view(intermim_shape).transpose(1,2)
+
+            weight = q @ k.transpose(-1,-2)   
+            
+            weight = weight / math.sqrt(self.d_head)
+
+            weight = F.softmax(weight, dim =-1)
+
+            output = weight @ v
+            
+            output = output.transpose(1,2).reshape(x_shape)
+
+            output = self.out_proj(output)
+
+            return output
+
+
+
 
             
 
